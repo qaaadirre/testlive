8a88c0e8ca51ed56618c69a3b96908de7757a053
qaaadirre <k007cart@gmail.com>
2025-10-23 18:01:24 +0530

    Update ImageSlideshow.js

--- GALLERY 2/src/components/ImageSlideshow.js
+++ GALLERY 2/src/components/ImageSlideshow.js
@@ -1,37 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ImageSlideshow = () => {
-  const photosPath = process.env.REACT_APP_PHOTOS_PATH;
+  const photosPath = process.env.REACT_APP_PHOTOS_PATH || 'https://raw.githubusercontent.com/qaaadirre/INDOGF/main/qaaadir';
   const images = [
     `${photosPath}/photo1.jpg`,
     `${photosPath}/photo2.jpg`,
-    `${photosPath}/photo3.jpg`,
-    `${photosPath}/photo4.jpg`,
-    `${photosPath}/photo5.jpg`,
-    `${photosPath}/photo6.jpg`
+    `${photosPath}/photo3.jpg`
   ];
-  const captions = [
+
+  const texts = [
     "Hello, little cutie~",
     "When we first met, the girl's serious little eyes, hehe",
-    "I was also comparing my feelings at that time~(. ・ω・。) ノ♡",
-    "It's my first time to go to Chongqing to meet a girl, and a little fairy appears",
-    "By the river, the moonlight is so beautiful tonight",
-    "Walking with claws, happy~o(≧v≦)o"
+    "I really like the bunny and carrot given by the girl!!!"
   ];
 
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex(prev => (prev + 1) % images.length);
+      setIndex(prev => (prev+1) % images.length);
     }, 2500);
     return () => clearInterval(interval);
   }, [images.length]);
 
   return (
-    <div id="imgTxt">
-      <span id="Txt">{captions[index]}</span>
-      <img src={images[index]} alt="slideshow" id="img" />
+    <div id="imgTxt" style={{position:'relative', zIndex:1, marginTop:'50px'}}>
+      <img src={images[index]} alt="photo" style={{
+        width:'320px', height:'400px', borderRadius:'16px',
+        boxShadow:'0 8px 25px rgba(255,105,135,0.3)',
+        objectFit:'contain', backdropFilter:'blur(5px)',
+      }} />
+      <div id="Txt" style={{marginTop:'10px', textAlign:'center'}}>
+        {texts[index]}
+      </div>
     </div>
   );
 };
