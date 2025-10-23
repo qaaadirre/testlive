a49e7f0693671b230cf926aaab830c1866905e5b
qaaadirre <k007cart@gmail.com>
2025-10-23 15:32:13 +0530

    Update App.js

--- GALLERY 2/src/App.js
+++ GALLERY 2/src/App.js
@@ -1,31 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Timer from './components/Timer';
 import TypeWriter from './components/TypeWriter';
 import ImageSlideshow from './components/ImageSlideshow';
+import HeartBackground from './components/HeartBackground';
 import './styles.css';
 
 function App() {
-  const [showContent, setShowContent] = useState(true);
-
   return (
     <div className="App">
       <audio autoPlay loop>
         <source src="music/music.mp3" type="audio/mpeg" />
       </audio>
 
+      {/* Floating hearts */}
+      <HeartBackground />
+
+      {/* Center photo card */}
       <ImageSlideshow />
 
-      {showContent && (
-        <div id="content">
-          <h2>We have been together</h2>
-          <Timer />
-        </div>
-      )}
+      {/* Top timer */}
+      <div id="content">
+        <h2>We have been together</h2>
+        <Timer />
+      </div>
 
+      {/* Bottom typewriter */}
       <TypeWriter />
 
       <footer>
-        Made with ❤️ by <span>{process.env.REACT_APP_NAME || 'Qaaadir'}</span>
+        Made with ❤️ by <span>Qaaadir</span>
       </footer>
     </div>
   );
