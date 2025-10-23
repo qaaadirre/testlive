1d13a790934dc7eb45a6b8de35a92223b44e954f
qaaadirre <k007cart@gmail.com>
2025-10-23 18:00:59 +0530

    Update Timer.js

--- GALLERY 2/src/components/Timer.js
+++ GALLERY 2/src/components/Timer.js
@@ -2,34 +2,24 @@ import React, { useEffect, useState } from 'react';
 
 const Timer = () => {
   const dateEnv = process.env.REACT_APP_DATE || '2025-09-11,02-15';
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState('00');
-  const [minutes, setMinutes] = useState('00');
-  const [seconds, setSeconds] = useState('00');
+  const [time, setTime] = useState({d:0,h:0,m:0,s:0});
 
   useEffect(() => {
     const [datePart, timePart] = dateEnv.split(',');
     const [year, month, day] = datePart.split('-').map(Number);
     const [hour, minute] = timePart.split('-').map(Number);
-    const startDate = new Date(year, month - 1, day, hour, minute, 0);
+    const start = new Date(year, month-1, day, hour, minute, 0);
 
     const interval = setInterval(() => {
       const now = new Date();
-      const diff = now - startDate;
+      const t = now - start;
 
-      const d = Math.floor(diff / (1000 * 60 * 60 * 24));
-      let h = Math.floor((diff / (1000 * 60 * 60)) % 24);
-      let m = Math.floor((diff / (1000 * 60)) % 60);
-      let s = Math.floor((diff / 1000) % 60);
+      const d = Math.floor(t / (1000*60*60*24));
+      const h = Math.floor((t / (1000*60*60)) % 24);
+      const m = Math.floor((t / (1000*60)) % 60);
+      const s = Math.floor((t / 1000) % 60);
 
-      if (h < 10) h = '0' + h;
-      if (m < 10) m = '0' + m;
-      if (s < 10) s = '0' + s;
-
-      setDays(d);
-      setHours(h);
-      setMinutes(m);
-      setSeconds(s);
+      setTime({d,h,m,s});
     }, 1000);
 
     return () => clearInterval(interval);
@@ -37,10 +27,10 @@ const Timer = () => {
 
   return (
     <div id="timer">
-      <b>{days}</b> Days
-      <b>{hours}</b> Hours
-      <b>{minutes}</b> Minutes
-      <b>{seconds}</b> Seconds
+      <b>{time.d}</b> Days 
+      <b>{time.h}</b> Hours
+      <b>{time.m}</b> Minutes
+      <b>{time.s}</b> Seconds
     </div>
   );
 };
