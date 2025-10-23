73f1669d418b16a81aaa6438d4c49716cec59940
qaaadirre <k007cart@gmail.com>
2025-10-23 15:29:07 +0530

    Update Timer.js

--- GALLERY 2/src/components/Timer.js
+++ GALLERY 2/src/components/Timer.js
@@ -7,27 +7,40 @@ const Timer = () => {
   const [minutes, setMinutes] = useState('00');
   const [seconds, setSeconds] = useState('00');
 
-  const [datePart, timePart] = dateEnv.split(',');
-  const [year, month, day] = datePart.split('-').map(Number);
-  const [hour, minute] = timePart.split('-').map(Number);
-  const startDate = new Date(year, month - 1, day, hour, minute, 0);
-
   useEffect(() => {
+    const [datePart, timePart] = dateEnv.split(',');
+    const [year, month, day] = datePart.split('-').map(Number);
+    const [hour, minute] = timePart.split('-').map(Number);
+    const startDate = new Date(year, month - 1, day, hour, minute, 0);
+
     const interval = setInterval(() => {
       const now = new Date();
       const diff = now - startDate;
-      setDays(Math.floor(diff / 86400000));
-      setHours(String(Math.floor((diff / 3600000) % 24)).padStart(2, '0'));
-      setMinutes(String(Math.floor((diff / 60000) % 60)).padStart(2, '0'));
-      setSeconds(String(Math.floor((diff / 1000) % 60)).padStart(2, '0'));
+
+      const d = Math.floor(diff / (1000 * 60 * 60 * 24));
+      let h = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      let m = Math.floor((diff / (1000 * 60)) % 60);
+      let s = Math.floor((diff / 1000) % 60);
+
+      if (h < 10) h = '0' + h;
+      if (m < 10) m = '0' + m;
+      if (s < 10) s = '0' + s;
+
+      setDays(d);
+      setHours(h);
+      setMinutes(m);
+      setSeconds(s);
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [startDate]);
+  }, [dateEnv]);
 
   return (
     <div id="timer">
-      <b>{days}</b> Days <b>{hours}</b> Hours <b>{minutes}</b> Minutes <b>{seconds}</b> Seconds
+      <b>{days}</b> Days
+      <b>{hours}</b> Hours
+      <b>{minutes}</b> Minutes
+      <b>{seconds}</b> Seconds
     </div>
   );
 };
